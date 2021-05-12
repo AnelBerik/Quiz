bcd8ee329389444d7d2f15a5daf5ba98702dd1ee
Acer <anelber184@gmail.com>
2021-05-12 12:58:20 +0600

    initial commit

---
+++ app/src/main/java/com/example/quizapp/QuizBook.java
@@ -0,0 +1,19 @@
+package com.example.quizapp;
+
+public class QuizBook {
+    public static String[] questions = new String[]{
+            "Washington, D.C. is the capital of the United States of America.",
+            "The sun revolves around the Earth.",
+            "Christopher Columbus proved that the Earth was round.",
+            "Queen Elizabeth II and Marilyn Monroe were born the same year.",
+            "Thomas Edison invented the light bulb."
+
+    };
+    public static int[] images = new int[]{
+         R.drawable.photo3, R.drawable.sun, R.drawable.photo2, R.drawable.queen, R.drawable.lamp
+
+    };
+    public static boolean[] answers = new boolean[]{
+        true,false,false,true,false
+    };
+}
---
+++ app/src/main/java/com/example/quizapp/ResultsActivity.java
@@ -0,0 +1,56 @@
+package com.example.quizapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class ResultsActivity extends AppCompatActivity {
+
+    TextView mGrade, mFinalScore;
+    Button mRetryButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_results);
+        mGrade = (TextView)findViewById(R.id.grade);
+        mFinalScore = (TextView)findViewById(R.id.outOf);
+        mRetryButton = (Button)findViewById(R.id.retry);
+
+
+        Bundle bundle = getIntent().getExtras();
+        int score = bundle.getInt("finalScore");
+
+        mFinalScore.setText("You scored " + score + " out of " + QuizBook.questions.length);
+
+        if (score == 5){
+            mGrade.setText("Outstanding");
+        }else if (score == 4){
+            mGrade.setText("Good Work");
+        }else if (score == 3) {
+            mGrade.setText("Good Effort");
+        }else {
+            mGrade.setText("Go over your notes");
+        }
+
+        mRetryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(ResultsActivity.this, QuizActivity.class));
+                ResultsActivity.this.finish();
+            }
+        });
+
+
+    }
+}
